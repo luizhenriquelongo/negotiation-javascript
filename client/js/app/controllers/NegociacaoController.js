964a5d457fb3ba4b2fd12cf7b0242430b98b146a
luizhenriquelongo <longo.luizh@gmail.com>
2020-05-29 15:14:09 -0300

    finalizacao do client

--- client/js/app/controllers/NegociacaoController.js
+++ client/js/app/controllers/NegociacaoController.js
@@ -7,17 +7,26 @@ class NegociacaoController {
         this._inputQuantidade = $('#quantidade');
         this._inputValor = $('#valor');
         this._listaNegociacoes = new ListaNegociacoes();
-        
+
+        this._negociacoesView = new NegociacoesView($('#negociacoesView'));
+        this._negociacoesView.update(this._listaNegociacoes);
+
+        this._mensagem = new Mensagem();
+        this._mensagemView = new MensagemView($('#mensagemView'));
+        this._mensagemView.update(this._mensagem);
     }
     
     adiciona(event) {
         
         event.preventDefault();
-
+        
         this._listaNegociacoes.adiciona(this._criaNegociacao());
+        this._negociacoesView.update(this._listaNegociacoes);
+        
+        this._mensagem.texto = 'Negociação adicionada com sucesso!';
+        this._mensagemView.update(this._mensagem);
+        
         this._limpaFormulario();
-
-        console.log(this._listaNegociacoes.negociacoes);
     }
 
     _criaNegociacao() {
@@ -33,7 +42,7 @@ class NegociacaoController {
 
         this._inputData.value = ''
         this._inputQuantidade.value = 1
-        this._inputValue.value = 0.0
+        this._inputValor.value = 0.0
 
         this._inputData.focus();
 
